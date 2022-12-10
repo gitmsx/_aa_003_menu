bc14d04c355f42ee424b306ed2521eff396c6e1a
gitmsx <gitmsx@mail.ru>
2022-12-10 12:45:55 +0300

    Saved on 10.12.2022-12:45:55,13

---
+++ Assets/Scripts/ButtonText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ButtonText : MonoBehaviour
+{
+
+
+    GameObject _GameObject;
+    public TextMeshProUGUI _textMeshProText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        _textMeshProText.text = "324234werwegsdffghts\n <#DC143C>Забыт давным-давноs sh /ngfhfshrstyrty yrtsh f gh sdf";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
